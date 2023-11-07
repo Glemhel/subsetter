c456958ca39ae41ed213461d54cdae902f630042
Vladimir Bazilevich <v.bazilevich@innopolis.university>
2023-11-07 19:10:31 +0300

    Ignore NaNs on a metrics preprocessing phase

--- src/metrics_preprocessor.py
+++ src/metrics_preprocessor.py
@@ -26,11 +26,23 @@ if __name__ == "__main__":
     unique_urls = d.url.unique()
     repo_url_encoder = dict([(url, i) for i, url in enumerate(unique_urls)])
 
-    with open('analysis/url_encoder.json', 'w') as f:
+    with open('../analysis/url_encoder.json', 'w') as f:
         json.dump(repo_url_encoder, f)
 
     function_metrics = d.explode(function_metrics_list)
     function_metrics.replace({'url': repo_url_encoder}, inplace=True)
+    
+    # Drop NaNs report
+    nans_per_record = function_metrics.isna().sum(axis=1)
+    print(f'In total, there are {(nans_per_record > 0).sum()} records containing at least 1 NaN value')
+    print('Detailed report for each repository with a non-zero number of NaNs')
+    for repo in unique_urls:
+        repo_id = repo_url_encoder[repo]
+        repo_records = function_metrics[function_metrics['url'] == repo_id]
+        n_records = repo_records.shape[0]
+        nan_records = ((repo_records.isna().sum(axis=1) > 0).sum())
+        if nan_records > 0:
+            print(f'{repo}({repo_id}) contains {nan_records} (out of {n_records}) records with NaNs')
     
     function_metrics.dropna(inplace=True)
     
